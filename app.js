1055b3d13a614c3e32cf728f6d754663bb046ecd
Jesser360 <jesser360@gmail.com>
2017-03-04 20:15:01 -0800

    added 2nd trap and added text

--- app.js
+++ app.js
@@ -46,6 +46,12 @@ $(document).ready(function() {
         $('#p2').css('left', 15);
         $('#p1').show();
         $('#p2').show();
+        $('.icon').show();
+        $('.arrows').show();
+        $('#obamaWarning').text("Warning, beware of traps");
+        $('#trumpWarning').text("Warning, beware of traps");
+
+
       }
     })
 });
@@ -67,24 +73,25 @@ function timer() {
 function moveObama(go) {
     if (go === true) {
         $(document).bind('keydown', function(el) {
-            left = 37;
-            up = 38;
-            right = 39;
-            down = 40;
-            if (el.keyCode == left) {
+          a = 65;
+          w = 87;
+          d = 68;
+          s = 83;
+
+            if (el.keyCode == a) {
                 $('#p1').animate({
                     left: "-=250px"
                 })
-            } else if (el.keyCode == up) {
+            } else if (el.keyCode == w) {
                 $('#p1').animate({
                     top: "-=250px"
                 })
-            } else if (el.keyCode == right) {
+            } else if (el.keyCode == d) {
                 $('#p1').animate({
                     left: "+=250px"
                 })
                 winnerObama();
-            } else if (el.keyCode == down) {
+            } else if (el.keyCode == s) {
                 $('#p1').animate({
                     top: "+=250px"
                 })
@@ -100,24 +107,24 @@ function moveObama(go) {
 function moveTrump(go) {
     if (go === true) {
         $(document).bind('keydown', function(el) {
-            a = 65;
-            w = 87;
-            d = 68;
-            s = 83;
-            if (el.keyCode == a) {
+          j = 74;
+          i = 73;
+          l = 76;
+          k = 75;
+            if (el.keyCode == j) {
                 $('#p2').animate({
                     left: "-=250px"
                 })
-            } else if (el.keyCode == w) {
+            } else if (el.keyCode == i) {
                 $('#p2').animate({
                     top: "-=250px"
                 })
-            } else if (el.keyCode == d) {
+            } else if (el.keyCode == l) {
                 $('#p2').animate({
                     left: "+=250px"
                 })
                 winnerTrump();
-            } else if (el.keyCode == s) {
+            } else if (el.keyCode == k) {
                 $('#p2').animate({
                     top: "+=250px"
                 })
@@ -137,6 +144,8 @@ function winnerObama() {
         $('.scoreObama').show()
         $('#p1').stop().hide();
         $('#p2').stop().hide();
+        $('.arrows').hide();
+        $('.icon').hide();
         winner = true;
         obamaCount++;
         if(obamaCount > trumpCount){
@@ -155,12 +164,15 @@ function winnerObama() {
           }
       }
    }
-   if(x >= 470 && x <= 670 && q <= 220){
+   if(x >= 470 && x <= 670 && q <= 215){
      $('#p1').stop();
-     console.log("obama Stopped");
-   } else if (q >= 245){
+     $('#obamaWarning').text("Go Around the Traps");
+   } else if (q >= 245 || q<= 95){
      $('#p1').stop();
-     console.log('obama stopped');
+     $('#obamaWarning').text("Stay in your Lane Obama!");
+   } else if(x >= 950 && x <=1050 && q >= 125){
+     $('#p1').stop();
+     $('#obamaWarning').text("Go Around the Traps");
    }
 }
 
@@ -172,6 +184,8 @@ function winnerTrump() {
         $('.scoreTrump').show()
         $('#p1').stop().hide();
         $('#p2').stop().hide();
+        $('.arrows').hide()
+        $('.icon').hide();
         winner = true;
         trumpCount++;
         if(trumpCount > obamaCount){
@@ -190,13 +204,16 @@ function winnerTrump() {
           }
      }
    }
-   if(y >= 430 && y <= 655 && r <= 500){
+   if(y >= 430 && y <= 655 && r <= 495){
      $('#p2').stop();
-     console.log("trump Stopped");
-   } else if (r >= 520){
+     $('#trumpWarning').text("Go Around the Traps");
+   } else if (r >= 525 || r<= 370){
      $('#p2').stop();
-     console.log('trump stopped');
-    }
+     $('#trumpWarning').text("Stay in your Lane Trump!");
+   } else if(y >= 940 && y <= 1040 && r >= 400){
+     $('#p2').stop();
+     $('#trumpWarning').text("Go Around the Traps");
+   }
 }
 // function blockObama(){
 //   var l = $('#p1').offset().left;
