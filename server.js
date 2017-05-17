e0fc2b5f6dfe35ebaf8d5ffb121bdf7131b720ee
Jesser360 <jesser360@gmail.com>
2017-05-17 13:41:14 -0700

    added server and express

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
-    app.listen(process.env.PORT || 3000)
+var express = require('express'),
+    app = express();
+
+var parallax = require('scrollmonitor-parallax');
+
+
+
+
+app.use(express.static(__dirname +'/public'));
+app.use('/js',express.static(path.join(__dirname, 'public/app.js')));
+app.use('/css',express.static(path.join(__dirname, 'public/main.css')));
+app.get('/', function index(req, res) {
+  res.sendFile('/index.html', {
+    root: __dirname
+  });
+});
+
+//=============
+//   Server
+//=============
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log('jesse portfolio is on 3000');
+});
